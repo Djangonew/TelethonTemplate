564b92a6cb8262d4a857c9a6787c716a424f5a23
Kadal <87251452+Djangonew@users.noreply.github.com>
2024-04-13 23:43:39 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -9,8 +9,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # Basics
-API_ID = config("API_ID", "20247370", default=None, cast=int)
-API_HASH = config("API_HASH", "813309fab8cd9fce260ce7269e543bdb", default=None)
-BOT_TOKEN = config("BOT_TOKEN", "6305234380:AAHPc-GGWKGc2b7Fs5_TDzh1xCVGTNUz5wA", default=None)
+API_ID = config("API_ID", "20247370", default=True, cast=int)
+API_HASH = config("API_HASH", "813309fab8cd9fce260ce7269e543bdb", default=True)
+BOT_TOKEN = config("BOT_TOKEN", "6305234380:AAHPc-GGWKGc2b7Fs5_TDzh1xCVGTNUz5wA", default=True)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
